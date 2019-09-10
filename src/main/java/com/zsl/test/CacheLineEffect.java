a3aec6f3a2ff69031c74b93867e5b65e8761f97b
曾轼麟 <zengshilin@qipeipu.com>
2019-09-10 16:48:01 +0800

    cache Line 测试

---
+++ src/main/java/com/zsl/test/CacheLineEffect.java
@@ -0,0 +1,50 @@
+package com.zsl.test;
+
+/**
+ * @program: finance-service
+ * @description: 测试 CPU cache Line 的性能
+ * 背景：Cache是由很多个cache line组成的。每个cache line通常是64字节，并且它有效地引用主内存中的一块儿地址。
+ * 一个Java的long类型变量是8字节，因此在一个缓存行中可以存8个long类型的变量。CPU每次从主存中拉取数据时，会把
+ * 相邻的数据也存入同一个cache line。在访问一个long数组的时候，如果数组中的一个值被加载到缓存中，它会自动加载
+ * 另外7个。因此你能非常快的遍历这个数组。事实上，你可以非常快速的遍历在连续内存块中分配的任意数据结构。下面的
+ * 例子是测试利用cache line的特性和不利用cache line的特性的效果对比。
+ * @author: ZengShiLin
+ * @create: 9/10/2019 3:47 PM
+ **/
+public class CacheLineEffect {
+
+    /**
+     * 考虑一般缓存行大小是64字节，一个 long 类型占8字节
+     * 二维数组, 1024 * 1024
+     */
+    static long[][] arr;
+
+    public static void main(String[] args) {
+        arr = new long[1024 * 1024][];
+        for (int i = 0; i < 1024 * 1024; i++) {
+            arr[i] = new long[8];
+            for (int j = 0; j < 8; j++) {
+                arr[i][j] = 0L;
+            }
+        }
+        long sum = 0L;
+        long marked = System.currentTimeMillis();
+        //遍历这个long数组 1024 * 1024 次 ,按照每次8个字节的方式读取 (8次刚好是一个缓存行)
+        for (int i = 0; i < 1024 * 1024; i += 1) {
+            for (int j = 0; j < 8; j++) {
+                sum = arr[i][j];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+
+        marked = System.currentTimeMillis();
+        //遍历这个数组8次,每次按照 1024 * 1024 的方式读取  (8次刚好是一个缓存行)
+        for (int i = 0; i < 8; i += 1) {
+            for (int j = 0; j < 1024 * 1024; j++) {
+                sum = arr[j][i];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+    }
+
+}
