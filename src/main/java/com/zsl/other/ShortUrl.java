57c3f9f3799b8ee483be1a8f08245a56c0870f02
曾轼麟 <zengshilin@qipeipu.com>
2019-11-09 11:09:21 +0800

    跳表和短地址生成器

---
+++ src/main/java/com/zsl/other/ShortUrl.java
@@ -0,0 +1,16 @@
+package com.zsl.other;/**
+ * Create by ZengShiLin on 2019-11-03
+ */
+
+/**
+ * @program: finance-service
+ * @description: 短网址发号器
+ * @author: ZengShiLin
+ * @create: 2019-11-03 下午15:08
+ **/
+public class ShortUrl {
+
+
+    
+
+}
